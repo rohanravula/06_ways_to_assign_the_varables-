346d245159b4cc3b8f65884389460b97e7f8638e
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-07-27 01:29:29 +0530

    Add files via upload

---
+++ ways_to_assign_variables.py
@@ -0,0 +1,49 @@
+'first way'
+# a=3
+# b=6
+# c=9
+# print(a)
+# print(b)
+# print(c)
+# print(d) #Name Error: name 'd' is not defined.. this is 1st method its lengthy way.
+
+'second way' #to take the variables in a single line
+# a=8 ; b=2 ; c=4
+# print(b)
+# print(c)
+# print(a)
+
+"""Third way""" #to reduce the print statement. writing the print statement in a single line
+# a=8 ; b=12 ; c=17
+# print(b) ; print(a) ; print(c)
+
+"""Fourth way"""
+# a,b=,2,3 #if there are only 2 varables and 3 numbers the it will show in output value error
+# a,b,c,d=1,2,3 #if there are 4 varables and 3 values then the output will be 
+# a,b,c=2,1,8 #we got to know that how many varables we are assining that many valuses we need to give
+
+"""data type Note:-A varable can hold the multiple values"""
+
+# a=1,2,3,4,5
+# print(a)
+# print(a,type(a)) #this data type is known as tuple
+
+# b=[5,6,7,8]
+# print(b)
+# print(b,type(b)) #if it is in square bracket then this data type is know as the 'list'
+
+"""fifth way"""
+a=10;b=20;c=30
+# print(a,b,c)
+# print(c,b,a)  
+# print(a,a,a,a,) #we can print the value how many times we can want.
+
+"""sixth way""" #wif the vales are dublicate means some varable are holding the same values.
+a=b=c=d=e=9
+# print(a) ; print(b) ; print(c) ; print(d) ; print(e) #this answer shows in different lines
+# print(a,b,c,d,e) #this answer shows in a slingle line
+
+"""Seventh way""" #if there are two varables are having the same values reamining are having different vales we can assign the values in this way.
+a=d=4;b=5;c=15;e=1
+print(a,c,b,e)
+ 
